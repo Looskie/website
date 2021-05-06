4eab7fefc54c85efc15688d13886b24081c0d63b
looskie <50378828+Looskie@users.noreply.github.com>
2021-05-06 07:59:25 -0400

    Optimizations: Remove ? as we're checking for them now

--- src/components/spotify.tsx
+++ src/components/spotify.tsx
@@ -9,14 +9,14 @@ export const Spotify = () => {
   return (
     <div className='spotify'>
       <img
-        src={lanyard.spotify?.album_art_url}
-        alt={lanyard.spotify?.song}
+        src={lanyard.spotify.album_art_url}
+        alt={lanyard.spotify.song}
         className='spotifyAlbumArt'
       />
       <div className='spotifyDetails'>
         <span className='listeningNow'>listening now...</span>
-        <h1 className='spotifySong'>{lanyard.spotify?.song}</h1>
-        <span className='spotifyArtist'>{lanyard.spotify?.artist}</span>
+        <h1 className='spotifySong'>{lanyard.spotify.song}</h1>
+        <span className='spotifyArtist'>{lanyard.spotify.artist}</span>
       </div>
     </div>
   );
