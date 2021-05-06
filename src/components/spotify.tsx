7eacebd0fcbdc9304c22f3757a4484ae057fb069
looskie <50378828+Looskie@users.noreply.github.com>
2021-05-06 00:11:14 -0400

    Fix: Dont let container show when not listening to anything

--- src/components/spotify.tsx
+++ src/components/spotify.tsx
@@ -4,19 +4,19 @@ import { Consts } from "../misc/consts";
 export const Spotify = () => {
   const { data: lanyard } = useLanyard(Consts.discordID);
 
-  if (!lanyard) return null;
+  if (!lanyard || !lanyard.spotify) return null;
 
   return (
-    <div className='spotify'>
+    <div className="spotify">
       <img
         src={lanyard.spotify?.album_art_url}
         alt={lanyard.spotify?.song}
-        className='spotifyAlbumArt'
+        className="spotifyAlbumArt"
       />
-      <div className='spotifyDetails'>
-        <span className='listeningNow'>listening now...</span>
-        <h1 className='spotifySong'>{lanyard.spotify?.song}</h1>
-        <span className='spotifyArtist'>{lanyard.spotify?.artist}</span>
+      <div className="spotifyDetails">
+        <span className="listeningNow">listening now...</span>
+        <h1 className="spotifySong">{lanyard.spotify?.song}</h1>
+        <span className="spotifyArtist">{lanyard.spotify?.artist}</span>
       </div>
     </div>
   );
