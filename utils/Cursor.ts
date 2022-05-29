ac087b73b41547f71be5418f92ce358cab8abd2f
Cody Miller <hello@looskie.com>
2022-05-29 08:39:13 -0400

    kissy

--- utils/Cursor.ts
+++ utils/Cursor.ts
@@ -13,52 +13,45 @@ export function loadCursor(ball: HTMLDivElement) {
   let ballX = x;
   let ballY = y;
 
-  function drawBall() {
+  const drawBall = () => {
     ballX += (x - ballX) * 0.1 - 1;
     ballY += (y - ballY) * 0.1 - 1;
 
     ball.style.top = `${ballY - window.scrollY}px`;
     ball.style.left = `${ballX}px`;
-  }
+  };
 
-  function loop() {
+  const loop = () => {
     drawBall();
     requestAnimationFrame(loop);
-  }
+  };
 
   loop();
 
-  function touch(event: TouchEvent) {
+  const touch = (event: TouchEvent) => {
     x = event.touches[0].pageX;
     y = event.touches[0].pageY;
-  }
+  };
 
-  function mousemove(event: MouseEvent) {
-    ball.style.opacity = "1";
-
+  const mouseMove = (event: MouseEvent) => {
     x = event.pageX;
     y = event.pageY;
-  }
+  };
 
-  function mousedown() {
-    ball.style.transform = "scale(1.5)";
-  }
-
-  function mouseup() {
-    ball.style.transform = "scale(1)";
-  }
+  const mouseDown = () => (ball.style.transform = "scale(1.5)");
+  const mouseUp = () => (ball.style.transform = "scale(1)");
 
   window.addEventListener("touchstart", touch);
   window.addEventListener("touchmove", touch);
-  window.addEventListener("mousemove", mousemove);
-  window.addEventListener("mousedown", mousedown);
-  window.addEventListener("mouseup", mouseup);
+  window.addEventListener("mousemove", mouseMove);
+  window.addEventListener("mousedown", mouseDown);
+  window.addEventListener("mouseup", mouseUp);
 
   return () => {
     window.removeEventListener("touchstart", touch);
     window.removeEventListener("touchmove", touch);
-    window.removeEventListener("mousemove", mousemove);
-    window.removeEventListener("mousedown", mousedown);
-    window.removeEventListener("mouseup", mouseup);
+    window.removeEventListener("mousemove", mouseMove);
+    window.removeEventListener("mousedown", mouseDown);
+    window.removeEventListener("mouseup", mouseUp);
   };
 }
