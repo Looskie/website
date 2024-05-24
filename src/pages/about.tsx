33e9084ade5ce08bb044d3e6ecf7c2ba0f520669
Cody Miller <hello@looskie.com>
2024-05-24 13:53:25 -0400

    oops

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -23,7 +23,7 @@ export default function About() {
         <AnimatedText element="h1" text="About" />
       </div>
       <p>
-        hey, i'm cody, an 19 year old software engineer and designer based in
+        hey, i'm cody, a 19 year old software engineer and designer based in
         the united states. i strive to build unique and polished web interfaces
         for both consumer and business ends. i love lacrosse, capybaras, and
         writing open source software!
